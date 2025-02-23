c407460597fdc1dc3b3fadbd374d2522c534a99f
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-02-23 21:30:38 +0400

    Add check out page  - added a page to display check out message  - linked check out button to the check out page  - small bug patches

---
+++ src/app/components/check-out/check-out.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-check-out',
+  imports: [RouterLink],
+  templateUrl: './check-out.component.html',
+  styleUrl: './check-out.component.css',
+})
+export class CheckOutComponent {}
